c1f458ca3bc6d02ed5664ed3f8c505c11d9c6d47
lucierlabs <neal.a.lucier@gmail.com>
2026-07-26 23:59:48 -0400

    update auth-jwt-github with main.tf and required locals, proivders, and terraform

--- auth-jwt-github/locals.tf
+++ auth-jwt-github/locals.tf
@@ -2,6 +2,27 @@ locals {
   input_csv  = file("${path.module}/../${var.input_file}")
   input_data = csvdecode("${local.input_csv}")
 
+  input_list = [
+    for row in local.input_data : {
+      app        = row.application
+      sub        = row.sub-application
+      env        = row.environment
+      perms      = row.permissions
+      repo       = row.repository
+      repo_owner = row.repo-owner
+      repo_env   = row.repo-environment
+      repo_sub   = "repo:${row.repo-owner}/${row.repository}:environment:${row.repo-environment}"
+    }
+  ]
+
+  repo_sub_list = [
+    for row in local.input_list : row.repo_sub
+  ]
+
+  repo_aud_list = [
+    for row in local.input_list : "https://github.com/${row.repo_owner}"
+  ]
+
   app_input_list = [
     for row in local.input_data : {
       app      = row.application
@@ -23,5 +44,4 @@ locals {
     } if row.sub-application != ""
   ]
 
-  
 }
---
+++ auth-jwt-github/main.tf
@@ -0,0 +1,21 @@
+resource "vault_jwt_auth_backend" "jwt_github" {
+  path               = "jwt-github"
+  description        = "GitHub JWT Auth Method"
+  oidc_discovery_url = "https://token.actions.githubusercontent.com"
+  bound_issuer       = "https://token.actions.githubusercontent.com"
+}
+
+resource "vault_jwt_auth_backend_role" "app_default_role" {
+  backend   = vault_jwt_auth_backend.jwt_github.path
+  role_name = "app-default-role"
+  role_type = "jwt"
+
+  user_claim = "sub"
+
+  bound_audiences = local.repo_aud_list
+  bound_claims = {
+    sub = join(",", local.repo_sub_list)
+  }
+
+  token_policies = []
+}
